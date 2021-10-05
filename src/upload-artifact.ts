6b8428a4f437dbad4824d77c5e790dc5d097012e
Josh Soref <jsoref@users.noreply.github.com>
2021-10-04 22:19:35 -0400

    Clarify which part of the error refers to a path

--- src/upload-artifact.ts
+++ src/upload-artifact.ts
@@ -13,19 +13,19 @@ async function run(): Promise<void> {
       switch (inputs.ifNoFilesFound) {
         case NoFileOptions.warn: {
           core.warning(
-            `No files were found with the provided path: ${inputs.searchPath}. No artifacts will be uploaded.`
+            `No files were found with the provided path: \`${inputs.searchPath}\`. No artifacts will be uploaded.`
           )
           break
         }
         case NoFileOptions.error: {
           core.setFailed(
-            `No files were found with the provided path: ${inputs.searchPath}. No artifacts will be uploaded.`
+            `No files were found with the provided path: \`${inputs.searchPath}\`. No artifacts will be uploaded.`
           )
           break
         }
         case NoFileOptions.ignore: {
           core.info(
-            `No files were found with the provided path: ${inputs.searchPath}. No artifacts will be uploaded.`
+            `No files were found with the provided path: \`${inputs.searchPath}\`. No artifacts will be uploaded.`
           )
           break
         }
